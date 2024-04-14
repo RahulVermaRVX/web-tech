16e04e93b7f59482cd9cfde8e22b9be6924bb495
rahulxxl <rahulvermaxxl@gmail.com>
2024-04-14 22:38:23 +0530

    Added Login Page in Servlets

--- java-servlets/src/java/login/Login.java
+++ java-servlets/src/java/login/Login.java
@@ -26,10 +26,50 @@ public class Login extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             // Login  Page Here...
+            String page = "<!DOCTYPE html>\n"
+                    + "<html lang='en'>\n"
+                    + "\n"
+                    + "<head>\n"
+                    + "    <meta charset='UTF-8'>\n"
+                    + "    <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n"
+                    + "    <title>Login</title>\n"
+                    + "    <style>\n"
+                    + "        .main { width: 50%; margin: 50px auto; }\n"
+                    + "        .title{ margin:20px; }\n"
+                    + "        .form-border { width: 50%; margin:10px; padding: 10px; }\n"
+                    + "        .form-element{ margin:5px; padding: 5px; }\n"
+                    + "        .ele-spacer{ width: 50%; display: inline; margin: 10px; padding: 10px; margin-right: auto; }\n"
+                    + "        .button{ margin: 5px 30px; padding: 2px 2px; }\n"
+                    + "    </style>\n"
+                    + "</head>\n"
+                    + "\n"
+                    + "<body>\n"
+                    + "    <div class='main'>\n"
+                    + "        <h1 class='title'>Please Login</h1>\n"
+                    + "        <div class='form-border'>\n"
+                    + "            <form action='/java-servlet/login-check' method='POST'>\n"
+                    + "                <div class='form-element'>\n"
+                    + "                   <div class='ele-spacer'><label for='user'>Username</label></div>\n"
+                    + "                   <div class='ele-spacer'><input type='text' id='user' name='uname'></div>\n"
+                    + "                </div>\n"
+                    + "                <div class='form-element'>\n"
+                    + "                    <div class='ele-spacer'><label for='pass'>Password</label></div>\n"
+                    + "                    <div class='ele-spacer'><input type='password' id='pass' name='passwd'></div>\n"
+                    + "                </div>\n"
+                    + "                <div>\n"
+                    + "                    <button class='button' type='submit'>Login</button>\n"
+                    + "                </div>\n"
+                    + "            </form>\n"
+                    + "        </div>\n"
+                    + "    </div>\n"
+                    + "</body>\n"
+                    + "\n"
+                    + "</html>";
+            out.println(page);
+
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -66,6 +106,6 @@ public class Login extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
